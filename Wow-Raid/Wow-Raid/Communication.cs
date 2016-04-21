88cfed66c1c64a8017740953139804b58aac0dc9
CJ Miller <cjmiller721@gmail.com>
2016-04-21 03:46:51 -0400

    Initial + Basic File Passing

---
+++ Wow-Raid/Wow-Raid/Communication.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wow_Raid
+{
+    class Communication
+    {
+        public static void sendFile(String filePath)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(Properties.Settings.Default.ServerIP, Constants.FILE_PORT);
+            client.Client.SendFile(filePath);
+            client.Close();
+        }
+    }
+}
