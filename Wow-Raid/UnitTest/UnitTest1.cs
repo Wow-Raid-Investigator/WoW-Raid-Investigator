9886f31e840ed985e9056319a0eb068dd08a72d6
CJ Miller <cjmiller721@gmail.com>
2016-05-01 23:15:01 -0400

    Adding small test project

---
+++ Wow-Raid/UnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wow_Raid;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPerstBasic()
+        {
+            DateTime time = DateTime.Now;
+
+            Perst.Instance["TEST_KEY"] = time;
+
+            Assert.AreEqual(time, Perst.Instance["TEST_KEY"]);
+        }
+
+        [TestMethod]
+        public void ComplexPerstObject()
+        {
+            String[] array = new String[] {"A", "B", "C", "D", "E" };
+
+            Perst.Instance["COMPLEX_KEY"] = array;
+         
+            String[] returned = (String[]) Perst.Instance["COMPLEX_KEY"];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(array[i], returned[i]);
+            }
+        }
+
+    }
+}
