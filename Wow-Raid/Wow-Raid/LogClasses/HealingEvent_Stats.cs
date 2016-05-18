bc8888db7a7a9a4e881c141a72f4cdf4b9f58bff
Ayuk23 <ayuk23@gmail.com>
2016-05-18 19:41:06 -0400

    Created HealingEvent_Stats.cs

---
+++ Wow-Raid/Wow-Raid/LogClasses/HealingEvent_Stats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wow_Raid.Stat;
+
+namespace Wow_Raid.LogClasses
+{
+    public partial class HealingEvent
+    {
+        public static long totalHealing(HealingEvent[] events)
+        {
+            return events.Sum(x => x.healing);
+        }
+
+        public static IEnumerable<UnitTotalHealing> groupBySource(HealingEvent[] events)
+        {
+            return events.GroupBy(x => x.Source).Select(p => new UnitTotalHealing(p.Key, p.Sum(d => d.healing)));
+        }
+    }
+}
